fcaf9f48bdb8407f09db418d289c914b03199378
Arthur Queiroz <tuckqueiroz82@gmail.com>
2021-02-16 20:31:23 -0300

    Commit Inicial

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Gastos</h1>
+    <table>
+        <tr>
+            <th>Data</th>
+            <th>Descrição</th>
+            <th>Valor</th>
+        </tr>
+        <tr>
+            <td>12/02</td>
+            <td>Passagem de ônibus</td>
+            <td>R$ 4,50</td>
+        </tr>
+        <tr>
+            <td>14/02</td>
+            <td>Salgado</td>
+            <td>R$ 1,50</td>
+        </tr>
+    </table>
+</body>
+</html>
